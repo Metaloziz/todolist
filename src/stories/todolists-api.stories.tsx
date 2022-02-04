e2b037b63e3601dd8f28ad5d40f6a87fbb9b3011
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-02-04 12:45:40 +0300

    13 lesson delete, for homework

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react'
-import {todolistAPI} from "../api/todolist-api";
 
 
 export default {
@@ -11,9 +10,6 @@ export const GetTodolists = () => {
 
     useEffect(() => {
 
-        todolistAPI.getTodos().then((res) => {
-            setState(res.data);
-        })
 
     }, [])
 
@@ -22,13 +18,11 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
 
-    let newToDoListTitle = 'newList'
+
 
     useEffect(() => {
 
-        todolistAPI.createTodo(newToDoListTitle).then((res) => {
-            setState(res.data);
-        })
+
     }, [])
 
     return <div> {JSON.stringify(state)}</div>
@@ -38,13 +32,10 @@ export const CreateTodolist = () => {
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
 
-    let listID = "b9bea611-ccb3-4bbd-aca5-88f1c9fd8724"
+
 
     useEffect(() => {
-        todolistAPI.deleteTodo(listID)
-            .then((res) => {
-                setState(res.data);
-            })
+
 
     }, [])
 
@@ -52,15 +43,10 @@ export const DeleteTodolist = () => {
 }
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
+
     useEffect(() => {
 
-        let listID = "764526c9-933e-4c88-8336-320d61d61a69"
 
-        let newTitle = 'new title111111111111'
-
-        todolistAPI.updateTodoTitle(listID, newTitle).then((res) => {
-            setState(res.data);
-        })
 
 
     }, [])
