aa80dcda6b0f8a5537db2caa9a9016d5aba79d7a
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-02-17 19:51:49 +0300

    add LinearProgress for all thunks

--- src/features/TodolistsList/todolists-reducer.ts
+++ src/features/TodolistsList/todolists-reducer.ts
@@ -61,25 +61,31 @@ export const fetchTodolistsTC = () => {
 }
 export const removeTodolistTC = (todolistId: string) => {
   return (dispatch: Dispatch<ActionsType>) => {
+    dispatch(appLoadingAC("loading"))
     todolistsAPI.deleteTodolist(todolistId)
       .then((res) => {
         dispatch(removeTodolistAC(todolistId))
+        dispatch(appLoadingAC("succeeded"))
       })
   }
 }
 export const addTodolistTC = (title: string) => {
   return (dispatch: Dispatch<ActionsType>) => {
+    dispatch(appLoadingAC("loading"))
     todolistsAPI.createTodolist(title)
       .then((res) => {
         dispatch(addTodolistAC(res.data.data.item))
+        dispatch(appLoadingAC("succeeded"))
       })
   }
 }
 export const changeTodolistTitleTC = (id: string, title: string) => {
   return (dispatch: Dispatch<ActionsType>) => {
+    dispatch(appLoadingAC("loading"))
     todolistsAPI.updateTodolist(id, title)
       .then((res) => {
         dispatch(changeTodolistTitleAC(id, title))
+        dispatch(appLoadingAC("succeeded"))
       })
   }
 }
