e3e1053b037e923df8147828155e16f811aab421
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-03-15 17:52:57 +0300

    19 lesson, 60%, login refactoring

--- src/api/todolists-api.ts
+++ src/api/todolists-api.ts
@@ -3,11 +3,11 @@ import axios from 'axios'
 const settings = {
   withCredentials: true,
   headers: {
-    'API-KEY': '6d28cae1-0bac-4f22-abce-7f5967410738'
+    'API-KEY': process.env.REACT_APP_API_KEY
   }
 }
 const instance = axios.create({
-  baseURL: 'https://social-network.samuraijs.com/api/1.1',
+  baseURL: process.env.REACT_APP_BASE_URL,
   ...settings
 })
 
--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,20 +1,52 @@
 import {Dispatch} from 'redux'
-import {setAppStatusAC} from '../../app/app-reducer'
-import {authAPI, LoginParamsType} from '../../api/todolists-api'
+import {setAppStatusAC} from 'app/app-reducer'
+import {authAPI, LoginParamsType} from 'api/todolists-api'
 import {
   handleServerAppError,
   handleServerNetworkError
-} from '../../utils/error-utils'
-import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+} from 'utils/error-utils'
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {AxiosError} from "axios";
 
-const initialState = {
-  isLoggedIn: false
-}
 
+export const loginTC = createAsyncThunk('auth/loginTC', async (data: LoginParamsType, {dispatch}) => {
+  dispatch(setAppStatusAC({status: 'loading'}))
+
+  try {
+    let responce = await authAPI.login(data)
+    if (responce.data.resultCode === 0) {
+      dispatch(setAppStatusAC({status: 'succeeded'}))
+      dispatch(setIsLoggedInAC({isLogin: true}))
+    } else {
+      handleServerAppError(responce.data, dispatch)
+    }
+  } catch (error) {
+    const {response, message} = error as AxiosError;
+    handleServerNetworkError({message}, dispatch)
+  }
+})
+
+// export const loginTC_ = (data: LoginParamsType) => (dispatch: Dispatch) => {
+//   dispatch(setAppStatusAC({status: 'loading'}))
+//   authAPI.login(data)
+//     .then(res => {
+//       if (res.data.resultCode === 0) {
+//         dispatch(setIsLoggedInAC({isLogin: true}))
+//         dispatch(setAppStatusAC({status: 'succeeded'}))
+//       } else {
+//         handleServerAppError(res.data, dispatch)
+//       }
+//     })
+//     .catch((error) => {
+//       handleServerNetworkError(error, dispatch)
+//     })
+// }
 
 const slice = createSlice({
   name: 'auth', // на основании этого имени будут генерироваться типы actions
-  initialState,
+  initialState: {
+    isLoggedIn: false
+  },
   reducers: {
     setIsLoggedInAC(state, action: PayloadAction<{ isLogin: boolean }>) {
       state.isLoggedIn = action.payload.isLogin  // innerJs позволяет менять стейт буд-то мутируя его, остальное она сама сделает
@@ -25,37 +57,9 @@ const slice = createSlice({
 export const {setIsLoggedInAC} = slice.actions
 
 export const authReducer = slice.reducer
-// export const authReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
-//   switch (action.type) {
-//     case 'login/SET-IS-LOGGED-IN':
-//       return {...state, isLoggedIn: action.value}
-//     default:
-//       return state
-//   }
-// }
-
-// actions
-
-// export const setIsLoggedInAC = (value: boolean) =>
-//   ({type: 'login/SET-IS-LOGGED-IN', value} as const)
-//
 
 // thunks
-export const loginTC = (data: LoginParamsType) => (dispatch: Dispatch) => {
-  dispatch(setAppStatusAC({status: 'loading'}))
-  authAPI.login(data)
-    .then(res => {
-      if (res.data.resultCode === 0) {
-        dispatch(setIsLoggedInAC({isLogin: true}))
-        dispatch(setAppStatusAC({status: 'succeeded'}))
-      } else {
-        handleServerAppError(res.data, dispatch)
-      }
-    })
-    .catch((error) => {
-      handleServerNetworkError(error, dispatch)
-    })
-}
+
 export const logoutTC = () => (dispatch: Dispatch) => {
   dispatch(setAppStatusAC({status: 'loading'}))
   authAPI.logout()
