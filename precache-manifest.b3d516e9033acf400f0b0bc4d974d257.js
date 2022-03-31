0a096089efb2bbd1d37d4a1ad98301f8192f1ddf
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-03-31 13:33:03 +0300

    Updates

---
+++ precache-manifest.b3d516e9033acf400f0b0bc4d974d257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c15e55f18b374c6ea15ec8f413c87868",
+    "url": "/index.html"
+  },
+  {
+    "revision": "103a89aa64afa0f7006c",
+    "url": "/static/css/main.ff0e3b07.chunk.css"
+  },
+  {
+    "revision": "b853e9c1599992e97f8f",
+    "url": "/static/js/2.1f608c14.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/static/js/2.1f608c14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "103a89aa64afa0f7006c",
+    "url": "/static/js/main.7d84a1fa.chunk.js"
+  },
+  {
+    "revision": "b201489dcbcc9f77b2de",
+    "url": "/static/js/runtime-main.e19410a0.js"
+  }
+]);
