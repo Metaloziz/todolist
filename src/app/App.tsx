1890a44ac729f611bfd5e172738955f97ea3568a
Andrew Gaityukevich <AndrewGaity@yandex.by>
2022-04-07 01:59:56 +0300

    refactoring

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -16,7 +16,7 @@ import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppRootStateType} from './store'
 import {initializeAppTC, RequestStatusType} from './app-reducer'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom'
 import {Login} from '../features/Login/Login'
 import {logoutTC} from '../features/Login/auth-reducer'
 import {HashRouter} from 'react-router-dom';
@@ -48,30 +48,30 @@ function App({demo = false}: PropsType) {
   }
 
   return (
-    <HashRouter>
-      <div className="App">
-        <ErrorSnackbar/>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" color="inherit" aria-label="menu">
-              <Menu/>
-            </IconButton>
-            <Typography variant="h6">
-              News
-            </Typography>
-            {isLoggedIn &&
-              <Button color="inherit" onClick={logoutHandler}>Log out</Button>}
-          </Toolbar>
-          {status === 'loading' && <LinearProgress/>}
-        </AppBar>
-        <Container fixed>
-          <Routes>
-            <Route path={'/'} element={<TodolistsList demo={demo}/>}/>
-            <Route path={'/login'} element={<Login/>}/>
-          </Routes>
-        </Container>
-      </div>
-    </HashRouter>
+    <div className="App">
+      <ErrorSnackbar/>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <Menu/>
+          </IconButton>
+          <Typography variant="h6">
+            News
+          </Typography>
+          {isLoggedIn &&
+            <Button color="inherit" onClick={logoutHandler}>Log out</Button>}
+        </Toolbar>
+        {status === 'loading' && <LinearProgress/>}
+      </AppBar>
+      <Container fixed>
+        <Routes>
+          <Route path="/login" element={<Login/>}/>
+          <Route path={'/'} element={<TodolistsList demo={demo}/>}/>
+          <Route path="/404" element={<h1>404: PAGE NOT FOUND</h1>}/>
+          <Route path="*" element={<Navigate to="/404"/>}/>
+        </Routes>
+      </Container>
+    </div>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,15 @@ import * as serviceWorker from './serviceWorker';
 import App from './app/App';
 import {store} from './app/store';
 import {Provider} from 'react-redux';
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.render(
+  <HashRouter>
     <Provider store={store}>
-        <App/>
-    </Provider>, document.getElementById('root'));
+      <App/>
+    </Provider>
+  </HashRouter>
+  , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
